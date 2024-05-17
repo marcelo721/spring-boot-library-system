3874d864e7e2812b8d7f907d8ad5439ff1e19789
TANGOSOO <gabrielmmeireles@alu.ufc.br>
2024-05-17 20:52:02 -0300

    add password encoder to Instantiation

--- src/main/java/com/marceloHsousa/LibraryManagementSystem/config/Instantiation.java
+++ src/main/java/com/marceloHsousa/LibraryManagementSystem/config/Instantiation.java
@@ -4,21 +4,24 @@ import com.marceloHsousa.LibraryManagementSystem.entities.User;
 import com.marceloHsousa.LibraryManagementSystem.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public Instantiation(UserRepository userRepository) {
+    public Instantiation(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
         userRepository.deleteAll();
-        User gabe = new User("Gabriel meireles", "gabriel@gmail.com", "gabriel321", "");
+        User gabe = new User("Gabriel meireles", "gabriel@gmail.com", passwordEncoder.encode("gabe123"), "");
         userRepository.save(gabe);
     }
 }
