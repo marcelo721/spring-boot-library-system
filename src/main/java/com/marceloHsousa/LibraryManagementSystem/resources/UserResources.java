870a6592510a6ce897da973aeb5bab80fbe362a7
TANGOSOO <gabrielmmeireles@alu.ufc.br>
2024-05-17 21:03:48 -0300

    added endpoint to find user by id

--- src/main/java/com/marceloHsousa/LibraryManagementSystem/resources/UserResources.java
+++ src/main/java/com/marceloHsousa/LibraryManagementSystem/resources/UserResources.java
@@ -1,9 +1,13 @@
 package com.marceloHsousa.LibraryManagementSystem.resources;
 
 
+import com.marceloHsousa.LibraryManagementSystem.entities.User;
 import com.marceloHsousa.LibraryManagementSystem.services.UserService;
 import org.hibernate.internal.build.AllowNonPortable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +21,11 @@ public class UserResources {
     @Autowired
     public UserResources(UserService service) {
         this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id){
+        User user=service.findUserById(id);
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/marceloHsousa/LibraryManagementSystem/services/UserService.java
+++ src/main/java/com/marceloHsousa/LibraryManagementSystem/services/UserService.java
@@ -1,8 +1,11 @@
 package com.marceloHsousa.LibraryManagementSystem.services;
 
+import com.marceloHsousa.LibraryManagementSystem.entities.User;
 import com.marceloHsousa.LibraryManagementSystem.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -13,5 +16,14 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
+    }
+
+    public User findUserById(Long id){
+        Optional<User> user=userRepository.findById(id);
+        if(user.isPresent()){
+            return user.get();
+        }else{
+            throw new RuntimeException("User Not Found"); //placeholder
+        }
     }
 }
