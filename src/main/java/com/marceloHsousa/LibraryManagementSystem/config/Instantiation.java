45d158d84f23fb49d69f4a1a42c05077fd0cb869
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-17 22:43:12 -0300

    created list of users for testing in the database

--- src/main/java/com/marceloHsousa/LibraryManagementSystem/config/Instantiation.java
+++ src/main/java/com/marceloHsousa/LibraryManagementSystem/config/Instantiation.java
@@ -6,6 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
@@ -21,8 +24,13 @@ public class Instantiation implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         userRepository.deleteAll();
+        List<User> users = new ArrayList<>();
 
-        User gabe = new User("Gabriel meireles", "gabriel@gmail.com", passwordEncoder.encode("gabe123"), "ROLE_ADMIN");
-        userRepository.save(gabe);
+        users.add(new User("Gabriel Meireles", "gabriel@gmail.com", passwordEncoder.encode("gabe123"), "ROLE_ADMIN"));
+        users.add(new User("Ana Silva", "ana@gmail.com", passwordEncoder.encode("ana456"), "ROLE_USER"));
+        users.add(new User("João Santos", "joao@gmail.com", passwordEncoder.encode("joao789"), "ROLE_USER"));
+        users.add(new User("Maria Oliveira", "maria@gmail.com", passwordEncoder.encode("maria321"), "ROLE_ADMIN"));
+
+        userRepository.saveAll(users);
     }
 }
