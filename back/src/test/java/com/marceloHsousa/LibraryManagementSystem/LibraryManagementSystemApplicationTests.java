cabae5b7b542eca39bd5ffb8dea57699bd22272d
TANGOSOO <gabrielmmeireles@alu.ufc.br>
2024-05-21 13:40:40 -0300

    added the first test to the test class

--- back/src/test/java/com/marceloHsousa/LibraryManagementSystem/LibraryManagementSystemApplicationTests.java
+++ back/src/test/java/com/marceloHsousa/LibraryManagementSystem/LibraryManagementSystemApplicationTests.java
@@ -1,13 +1,63 @@
 package com.marceloHsousa.LibraryManagementSystem;
 
+import com.marceloHsousa.LibraryManagementSystem.entities.user.User;
+import com.marceloHsousa.LibraryManagementSystem.services.BookService;
+import com.marceloHsousa.LibraryManagementSystem.services.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+import javax.sql.DataSource;
+import java.util.Date;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestPropertySource("/application.properties")
+@SpringBootTest(classes = LibraryManagementSystemApplication.class)
 class LibraryManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private JdbcTemplate jdbc;
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@BeforeAll
+	public static void beforeAll(){
 	}
 
+
+	@BeforeEach
+	public void beforeEach(){
+		jdbc.execute("SET foreign_key_checks = 0;");
+		jdbc.execute("DELETE FROM books");
+		jdbc.execute("DELETE FROM loans");
+		jdbc.execute("DELETE FROM users");
+		jdbc.execute("DELETE FROM authors");
+		jdbc.execute("DELETE FROM book_category");
+		jdbc.execute("SET foreign_key_checks = 1;");
+	}
+
+	@AfterEach
+	public void afterEach(){
+	}
+
+	//User tests
+
+	@Test
+	public void crateUser(){
+		userService.save(new User(null, "Marcelim", "marcelo@gmail.com", passwordEncoder.encode("gabe123"), "ROLE_ADMIN", null));
+		User user=userService.findByEmail("marcelo@gmail.com");
+		assertEquals("marcelo@gmail.com", user.getEmail());
+	}
 }
